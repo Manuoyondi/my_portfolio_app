69b412927cfbbf0f7f48b188cad93760acdbb9d9
manu <oyondiimmanuel@hmail.com>
2020-05-09 19:11:55 +0300

    Updates

---
+++ precache-manifest.383e859b7be3f2f91214535052006eb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca2ca091f1656feb292b32260ec83f8",
+    "url": "/my_portfolio_app/index.html"
+  },
+  {
+    "revision": "d545fb959d1a26355aa6",
+    "url": "/my_portfolio_app/static/css/main.9c405617.chunk.css"
+  },
+  {
+    "revision": "dcb4ba6eee723e62a7d1",
+    "url": "/my_portfolio_app/static/js/2.35b5fa6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_portfolio_app/static/js/2.35b5fa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d545fb959d1a26355aa6",
+    "url": "/my_portfolio_app/static/js/main.ab7e1308.chunk.js"
+  },
+  {
+    "revision": "ea8ae7821ede3f1a86ff",
+    "url": "/my_portfolio_app/static/js/runtime-main.befa52f4.js"
+  }
+]);
